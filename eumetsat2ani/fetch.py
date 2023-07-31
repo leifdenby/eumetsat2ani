374c5ff4193e296b54043bfc8751f2a3a48ba39b
Leif Denby <leif@denby.eu>
2023-07-31 14:38:56 +0200

    Setup pre-commit

--- eumetsat2ani/fetch.py
+++ eumetsat2ani/fetch.py
@@ -1,21 +1,18 @@
-import eumdac
-
-import isodate
-import datetime
-import shutil
-import requests
-import time
-from pathlib import Path
-from loguru import logger
 import itertools
-import numpy as np
+import shutil
+from pathlib import Path
 
+import eumdac
+import isodate
+import numpy as np
+import requests
+from loguru import logger
 from satpy.resample import get_area_def
 
 
 def pyresample_area_definition_to_boundary_polygon_wkt(area):
     lons, lats = area.boundary().contour()
-    
+
     # use [min, max] combinations to get all 4 corners
     minmap_ops = [np.min, np.max]
     ops = itertools.product(minmap_ops, minmap_ops)
@@ -24,17 +21,23 @@ def pyresample_area_definition_to_boundary_polygon_wkt(area):
         lon_op, lat_op = op
         lonlat_corners = [lon_op(lons), lat_op(lats)]
         corners.append(lonlat_corners)
-    
+
     lons, lats = zip(*corners)
     lons = list(lons) + [lons[0]]
     lats = list(lats) + [lats[0]]
     pts_str = ", ".join(f"{lon} {lat}" for (lon, lat) in zip(lons, lats))
-    
+
     return f"POLYGON (({pts_str}))"
 
 
 def download_source_files(
-    api_key, api_secret, area_definition, collection_name, t_start, t_end, root_data_path
+    api_key,
+    api_secret,
+    area_definition,
+    collection_name,
+    t_start,
+    t_end,
+    root_data_path,
 ):
     token = eumdac.AccessToken((api_key, api_secret))
     try:
@@ -57,7 +60,7 @@ def download_source_files(
     root_data_path.mkdir(parents=True, exist_ok=True)
     logger.info(f"Found {len(products)} products")
     for product in products:
-        prod_identifier = product.metadata['properties']['identifier']
+        prod_identifier = product.metadata["properties"]["identifier"]
         fp_local_guess = root_data_path / f"{prod_identifier}.zip"
         if fp_local_guess.exists():
             logger.info(f"Skipping {fp_local_guess} as it already exists")
@@ -72,20 +75,27 @@ def download_source_files(
                     shutil.copyfileobj(fsrc, fdst)
                     logger.info(f"Downloaded {local_filepath}")
                     local_filepaths.append(local_filepath)
-                
+
 
 if __name__ == "__main__":
     import argparse
+
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--api-key", type=str, required=True)
     argparser.add_argument("--api-secret", type=str, required=True)
-    argparser.add_argument("--collection-name", type=str, default="EO:EUM:DAT:MSG:HRSEVIRI")
-    argparser.add_argument("--t-start", type=isodate.parse_datetime, default="2015-04-14T09:00:00")
-    argparser.add_argument("--t-end", type=isodate.parse_datetime, default="2015-04-14T13:00:00")
+    argparser.add_argument(
+        "--collection-name", type=str, default="EO:EUM:DAT:MSG:HRSEVIRI"
+    )
+    argparser.add_argument(
+        "--t-start", type=isodate.parse_datetime, default="2015-04-14T09:00:00"
+    )
+    argparser.add_argument(
+        "--t-end", type=isodate.parse_datetime, default="2015-04-14T13:00:00"
+    )
     argparser.add_argument("--area", type=str, default="euro")
     argparser.add_argument("--root-data-path", type=Path, default="data")
     args = argparser.parse_args()
-    
+
     # load area definition from satpy
     area_definition = get_area_def(args.area)
 
